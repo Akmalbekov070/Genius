2a736c149c4f591921d99cddf492c98d63d43408
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-12-18 21:04:13 +0300

    commit two

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
+import HeaderPage from '@/components/Header/Header';
+import HomePage from '@/components/HomePage/HomePage';
+import { Box } from '@chakra-ui/core';
+
 export default function Home() {
-	return <>Salom</>;
+	return (
+		<Box>
+			<HeaderPage />
+			<Box>
+				<HomePage />
+			</Box>
+		</Box>
+	);
 }
